e66c1c6099fada279c8466853cbb6efddbd9e086
Edipo Elwes <edipoelwes2@gmail.com>
2019-11-19 11:28:04 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+$n1 = rand(0, 10);
+$n2 = rand(0, 10);
+$_SESSION['v'] = $n1 + $n2;
+?>
+
+<h2>Verificador de humano</h2>
+
+<form action="verificador.php" method="post">
+    <?php echo "$n1 + $n2 ="?>
+
+    <input type="number" name="n">
+
+    <input type="submit" value="Verificar">
+</form>
+
